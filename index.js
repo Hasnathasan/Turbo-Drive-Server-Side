6b62552a9c4be4108b762d88d7ad7dd3c2f673b0
Hasnathasan <hasnatoooooooo@gmail.com>
2024-01-31 02:16:44 +0600

    Code formated

--- index.js
+++ index.js
@@ -25,68 +25,68 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        
+
         const galleryCollection = client.db('turbo-Drive').collection('gallery');
         const toyCollection = client.db('turbo-Drive').collection('toys');
         // const indexKeys = { productName: 1};
         // const indexOptions = { name: "productName"};
         // const result = await toyCollection.createIndex(indexKeys, indexOptions)
 
-        
-        
-        
-        
-        app.get('/gallery', async(req, res) => {
+
+
+
+
+        app.get('/gallery', async (req, res) => {
             const result = await galleryCollection.find().toArray();
             res.send(result)
         })
-        
-        app.get('/serchedJobs', async(req, res) => {
+
+        app.get('/serchedJobs', async (req, res) => {
             const serchText = req.query.serchedText;
             const email = req.query.email;
-            const result = await toyCollection.find({email: email, productName: {$regex: serchText, $options: "i"}}).toArray();
+            const result = await toyCollection.find({ email: email, productName: { $regex: serchText, $options: "i" } }).toArray();
             res.send(result)
         })
-        app.get('/toys', async(req, res) => {
+        app.get('/toys', async (req, res) => {
             console.log(req.query);
             let query = {};
             let sortedBy = {};
-            if(req.query.email){
-                query = {email: req.query.email};
-                if(req.query.sort){
-                    sortedBy = { price: parseInt(req.query.sort)};
+            if (req.query.email) {
+                query = { email: req.query.email };
+                if (req.query.sort) {
+                    sortedBy = { price: parseInt(req.query.sort) };
                 }
-            }else if(req.query.category){
-                query = {category: req.query.category}
+            } else if (req.query.category) {
+                query = { category: req.query.category }
             }
-            
+
             const result = await toyCollection.find(query).sort(sortedBy).limit(20).toArray();
             res.send(result)
         })
 
-        app.post('/toys', async(req, res) => {
+        app.post('/toys', async (req, res) => {
             const toy = req.body;
             const result = await toyCollection.insertOne(toy);
             res.send(result)
         })
 
 
-        app.get('/toys/:id', async(req, res) => {
+        app.get('/toys/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await toyCollection.findOne(query);
             res.send(result)
         })
-        app.delete('/toys/:id', async(req, res) => {
+        app.delete('/toys/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await toyCollection.deleteOne(query);
             res.send(result)
         })
-        app.patch('/toys/:id', async(req, res) => {
+        app.patch('/toys/:id', async (req, res) => {
             const id = req.params.id;
             const product = req.body;
-            const filter = { _id: new ObjectId(id)}
+            const filter = { _id: new ObjectId(id) }
             const updatedProduct = {
                 $set: {
                     price: product.price,
@@ -98,7 +98,7 @@ async function run() {
             res.send(result)
         })
 
-        
+
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
